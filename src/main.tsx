c79879b3a0589ea2c476f801c1f071286a980e38
Fernando Mirasol <fhermq@gmail.com>
2025-10-03 11:59:23 -0600

    Se agrega useState y se agrega funcionalidad para cambiar de color

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './HooksApp.tsx'
+// import { HooksApp } from './HooksApp.tsx'
+import { TrafficLight } from './01-useState/TrafficLight.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <TrafficLight/>
   </StrictMode>,
 )
