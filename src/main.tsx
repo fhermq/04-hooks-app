b162aff449b2c3dcce217e06def97ab17035c1f0
Fernando Mirasol <fhermq@gmail.com>
2025-10-03 19:02:47 -0600

    se agrega useCounterPokemon, usePokemon

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './HooksApp.tsx'
 // import { TrafficLight } from './01-useState/TrafficLight.tsx'
-import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook.tsx'
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook.tsx'
+import { PokemonPage } from './03-examples/PokemonPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <TrafficLight/> */}
     {/* <TrafficLightWithEffect/> */}
-    <TrafficLightWithHook/>
+    {/* <TrafficLightWithHook/> */}
+
+    <PokemonPage/>
   </StrictMode>,
 )
